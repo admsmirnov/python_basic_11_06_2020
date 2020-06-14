c3012fb1c898917f4224dfbd47e2b78e650265c0
admsmirnov <adm_smirnov@mail.ru>
2020-06-14 19:47:08 +0300

    HW les1

--- homeworks/les1/task3.py
+++ homeworks/les1/task3.py
@@ -1,2 +1,11 @@
 
-# 3
+# Ввод данных пользователя и их проверка
+digit = input('Введите любое число\n')
+if digit.isdigit():
+    digit = int(digit)
+    result = f'{digit} + {digit}{digit} + {digit}{digit}{digit}'
+    print(result)
+else:
+    print('Неверный ввод! Вводите только число!')
+
+
