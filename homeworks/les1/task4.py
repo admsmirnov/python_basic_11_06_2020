0a4d3ef2e7131255735357c1c671ee08159b97ef
admsmirnov <adm_smirnov@mail.ru>
2020-06-14 22:58:44 +0300

    HW les1

--- homeworks/les1/task4.py
+++ homeworks/les1/task4.py
@@ -1,2 +1,9 @@
 
-# 4
+while True:
+    plz = input('Введите целое положительное число\n')
+    if plz.isdigit():
+        plz=int(plz)
+        plz <= 0
+        break
+    print('Ошибка! Введите целое положительное число')
+
