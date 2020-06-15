cc161baa0c6260a86f40bf5b820b92a913102cb1
admsmirnov <adm_smirnov@mail.ru>
2020-06-15 18:10:01 +0300

    HW les1

--- homeworks/les1/task4.py
+++ homeworks/les1/task4.py
@@ -1,9 +1,18 @@
 
+# Проверка, что число положительное и целое
 while True:
     plz = input('Введите целое положительное число\n')
     if plz.isdigit():
         plz=int(plz)
         plz <= 0
         break
-    print('Ошибка! Введите целое положительное число')
+# Вычисление наибольшей цифры
+prm = plz % 10
+plz = plz // 10
+while plz > 0:
+    if plz % 10 > prm:
+        prm = plz % 10
+    plz = plz // 10
+print(prm)
+
 
